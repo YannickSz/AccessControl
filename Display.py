9aea634983f12f1aa41b25ddf819f732a09e047f
Yannick <yulz4121@gmail.com>
2022-05-17 14:01:30 +0200

    Initialisierung

---
+++ Display.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import ST7735
+import sys
+
+
+display = ST7735.ST7735(port=1, cs=0, dc=23, backlight=None, 
+rst=16, width=128, height=160, rotation=0, invert=False, 
+offset_left=0, offset_top=0 )
+image = Image.new('RGB', (display.width, display.height))
+draw = ImageDraw.Draw(image)
+
+
+#print (len(sys.argv))
+
+
+if len(sys.argv) < 2:
+    print ("Usage: Display.py <TEXT>")
+    sys.exit()
+
+
+draw.text((4, 30), "Welcome",
+font=ImageFont.truetype("codec.ttf", 25), fill=(255, 255, 255))
+
+
+draw.text((4, 90), sys.argv[1]+'!',
+font=ImageFont.truetype("codec.ttf", 25), fill=(255, 255, 255))
+display.display(image)
---
+++ ReaderTest.py
@@ -0,0 +1,27 @@
+import os
+from pirc522 import RFID
+
+rdr = RFID()
+
+while True:
+  os.system('python3 DisplayImage.py loading')
+  rdr.wait_for_tag()
+  (error, tag_type) = rdr.request()
+
+  if not error:
+    print("Tag detected")
+    (error, uid) = rdr.anticoll()
+
+    if not error:
+      print("UID: " + str(uid))
+
+      if uid==[26, 198, 157, 26, 91]:
+        os.system('python3 DisplayImage.py success')
+        os.system('python3 Display.py Serdar')
+
+      else: 
+        os.system('python3 DisplayImage.py denied')
+
+
+# Calls GPIO cleanup
+rdr.cleanup()
