9d8b28c7c21bb38ec30b3ef745319412008f3039
Serdar <serdar2002@live.de>
2022-05-18 12:39:34 +0200

    fixed Temperature script

--- Temperature.sh
+++ Temperature.sh
@@ -3,4 +3,4 @@
 temp="$(vcgencmd measure_temp | grep -oP '\d\d\.\d')"
 mac="$(cat /sys/class/net/eth0/address)"
 
-curl -X PUT h2948228.stratoserver.net -H 'Content-Type: application/json' -d '{"temperature":"'$temp'", "macAddress":"'$mac'"}'
+curl -X PUT h2948228.stratoserver.net:8080/putTemperature -H 'Content-Type: application/json' -d '{"temperature":"'$temp'", "macAddress":"'$mac'"}'
