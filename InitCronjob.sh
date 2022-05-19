0da45f2c5916da72deb62796700f7fa7d21760a6
Serdar <serdar2002@live.de>
2022-05-19 11:03:32 +0200

    improved init script

--- InitCronjob.sh
+++ InitCronjob.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 logname=$(logname)
+path=$(pwd)
 
-chmod -v 777 "/var/spool/cron/crontabs/"
-chmod -v 777 "/var/spool/cron/crontabs/$logname"
+if grep -q "$path/Temperature.sh" "/var/spool/cron/crontabs/$logname"
+then
+    echo "Skript ist schon vorhanden!"
+else 
+    chmod -v 777 "/var/spool/cron/crontabs/"
+    chmod -v 777 "/var/spool/cron/crontabs/$logname"
 
-echo "*/5 * * * * $(pwd)/Temperature.sh" >> "/var/spool/cron/crontabs/$logname"
+    echo "*/5 * * * * $path/Temperature.sh" >> "/var/spool/cron/crontabs/$logname"
 
-chmod -v 600 "/var/spool/cron/crontabs/$logname"
-chmod -v 730 "/var/spool/cron/crontabs/"
+    chmod -v 600 "/var/spool/cron/crontabs/$logname"
+    chmod -v 730 "/var/spool/cron/crontabs/"
+fi
