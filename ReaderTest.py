75d7fa33cc3dbea317e2904dbb50c2e5aefdeae5
Serdar <serdar2002@live.de>
2022-05-20 10:38:02 +0200

    added button functionality and small fixes

--- ReaderTest.py
+++ ReaderTest.py
@@ -3,6 +3,8 @@ import os
 import requests
 import json
 import datetime
+import pigpio
+import time as t
 
 rdr = RFID()
 urlLogin = "http://pumpkin.international:8080/login"
@@ -10,6 +12,18 @@ urlIsLoggedIn = "http://pumpkin.international:8080/isLoggedIn"
 headers = {'Content-Type': 'application/json'}
 mac = os.popen('cat /sys/class/net/eth0/address').read().replace("\n", "")
 format = '%Y-%m-%d %H:%M:%S'
+pi = pigpio.pi()
+
+# gpio 13 left
+# gpio 19 right
+def log_out():
+  os.system('python3 Display.py text Goodbye Goodbye')
+  while True:
+    if pi.read(13) == 1:
+      return "accept"
+    if pi.read(19) == 1:
+      return "cancel"
+    t.sleep(0.01)
 
 while True:
   os.system('python3 Display.py image loading')
@@ -23,31 +37,39 @@ while True:
     if not error:
       print("UID: " + str(uid))
       response = requests.get(urlIsLoggedIn, data = json.dumps({"rfid":str(uid)}), headers = headers)
-      if response.json().get("loggedIn") == False:
+      if response.json().get("loggedIn") == True:
+        time = datetime.datetime.strptime(response.json().get("lastLogin"), format)
+        delta = datetime.datetime.now() - time
+        seconds = int(delta.total_seconds())
+        if seconds >= 30:
+          if log_out() == "cancel":
+            continue
+          response = requests.post(urlLogin, data = json.dumps({"rfid":str(uid), "macAddress":str(mac)}), headers = headers)
+          if response.status_code == 200:
+            if response.json().get("message") == "Logout":
+              os.system('python3 Display.py image success')
+              os.system('python3 Display.py text Goodbye ' + response.json().get("user"))
+          elif response.status_code == 403: 
+            os.system('python3 Display.py image denied')
+          elif response.status_code == 409:
+            os.system('python3 Display.py text LoggedIn elsewhere')
+          else:
+            os.system('python3 Display.py image error')
+        else:
+          os.system('python3 Display.py text Wait ' + str(30 - seconds) + 's')
+      else:
         response = requests.post(urlLogin, data = json.dumps({"rfid":str(uid), "macAddress":str(mac)}), headers = headers)
         if response.status_code == 200:
           if response.json().get("message") == "Login":
             os.system('python3 Display.py image success')
             os.system('python3 Display.py text Welcome ' + response.json().get("user"))
-
-
-      elif response.json().get("loggedIn") == True:
-        time = datetime.datetime.strptime(response.json().get("lastLogin"), format)
-        delta = datetime.datetime.now() - time
-        seconds = int(delta.total_seconds())
-        if seconds >= 30:
-          response = requests.post(urlLogin, data = json.dumps({"rfid":str(uid), "macAddress":str(mac)}), headers = headers)
-          os.system('python3 Display.py image success')
-          os.system('python3 Display.py text Goodbye ' + response.json().get("user"))
+        elif response.status_code == 403: 
+          os.system('python3 Display.py image denied')
+        elif response.status_code == 409:
+          os.system('python3 Display.py text LoggedIn elsewhere')
         else:
-          os.system('python3 Display.py text Wait ' + str(30 - seconds) + 's')
-
-      elif response.status_code == 403: 
-        os.system('python3 Display.py image denied')
-      elif response.status_code == 409:
-        os.system('python3 Display.py text LoggedIn elsewhere')
-      else:
-        os.system('python3 Display.py image error')
+          os.system('python3 Display.py image error')
 
 # Calls GPIO cleanup
 rdr.cleanup()
+  
